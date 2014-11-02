0da33a1ec838d916f507af0739ae4a7a5362095a
bjarkihall <bjarkihall@hotmail.com>
2014-11-02 06:16:33 +0000

    Created urls.js
    Includes urls to all the sprites, shapes, sounds and stuff (changable if one would like to work on the project locally for example or if the links change, then we can modify it all at once in the same place).

---
+++ utils/urls.js
@@ -0,0 +1,92 @@
+var g_url_Brain = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Brain.png";
+var g_url_Child = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Child.png";
+var g_url_Dad = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Dad.png";
+var g_url_Enforcer = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Enforcer.png";
+var g_url_Grunt = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Grunt.png";
+var g_url_Hulk = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Hulk.png";
+var g_url_Mom = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Mom.png";
+var g_url_Player = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Player.png";
+var g_url_Prog = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Prog.png";
+var g_url_Quark = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Quark.png";
+var g_url_Spheroid = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Spheroid.png";
+var g_url_Tank = "https://notendur.hi.is/~eth31/cgp/staticdata/sprites/Tank.png";
+var g_url_2084 = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/2084.png";
+var g_url_BlackDiamond = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/BlackDiamond.png";
+var g_url_Checkers = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Checkers.png";
+var g_url_Diamond = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Diamond.png";
+var g_url_Dizzy = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Dizzy.png";
+var g_url_EnforcerSpark = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/EnforcerSpark.png";
+var g_url_extralife = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/extralife.png";
+var g_url_Rectangle = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Rectangle.png";
+var g_url_Skull = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Skull.png";
+var g_url_Spark = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Spark.png";
+var g_url_Square = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Square.png";
+var g_url_Triangle = "https://notendur.hi.is/~eth31/cgp/staticdata/Shapes/Triangle.png";
+var g_url_GreenAlphabet = "https://notendur.hi.is/~eth31/cgp/staticdata/Numbers%20and%20letters/GreenAlphabet.png";
+var g_url_RedAlphabet = "https://notendur.hi.is/~eth31/cgp/staticdata/Numbers%20and%20letters/RedAlphabet.png";
+var g_url_WhiteNumbers = "https://notendur.hi.is/~eth31/cgp/staticdata/Numbers%20and%20letters/WhiteNumbers.png";
+var g_url_HumanScore = "https://notendur.hi.is/~eth31/cgp/staticdata/Numbers%20and%20letters/HumanScore.png";
+var g_url_brains = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/brains.wav";
+var g_url_enfspark = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/enfspark.wav";
+var g_url_explode = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/explode.wav";
+var g_url_familypick = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/familypick.wav";
+var g_url_familypickrampage = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/familypickrampage.wav";
+var g_url_familydead = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/familydead.wav";
+var g_url_firstlevel = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/firstlevel.wav";
+var g_url_newlevel = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/newlevel.wav";
+var g_url_playerdead = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/playerdead.wav";
+var g_url_shot = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/shot.wav";
+var g_url_startsound = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/startsound.wav";
+var g_url_walk = "https://notendur.hi.is/~eth31/cgp/staticdata/Sounds/walk.wav";
+
+/* 
+	URLs:
+	nr. 01-12 are sprites (12)
+	nr. 13-24 are shapes (12)
+	nr. 25-28 are numbers and letters (4)
+	nr. 29-40 are sounds (12)
+
+	01 = g_url_Brain
+	02 = g_url_Child
+	03 = g_url_Dad
+	04 = g_url_Enforcer
+	05 = g_url_Grunt
+	06 = g_url_Hulk
+	07 = g_url_Mom
+	08 = g_url_Player
+	09 = g_url_Prog
+	10 = g_url_Quark
+	11 = g_url_Spheroid
+	12 = g_url_Tank
+
+	13 = g_url_2084
+	14 = g_url_BlackDiamond
+	15 = g_url_Checkers
+	16 = g_url_Diamond
+	17 = g_url_Dizzy
+	18 = g_url_EnforcerSpark
+	19 = g_url_extralife
+	20 = g_url_Rectangle
+	21 = g_url_Skull
+	22 = g_url_Spark
+	23 = g_url_Square
+	24 = g_url_Triangle
+
+	25 = g_url_GreenAlphabet
+	26 = g_url_RedAlphabet
+	27 = g_url_WhiteNumbers
+	28 = g_url_HumanScore
+
+	29 = g_url_brains
+	30 = g_url_enfspark
+	31 = g_url_explode
+	32 = g_url_familypick
+	33 = g_url_familypickrampage
+	34 = g_url_familydead
+	35 = g_url_firstlevel
+	36 = g_url_newlevel
+	37 = g_url_playerdead
+	38 = g_url_shot
+	39 = g_url_startsound
+	40 = g_url_walk
+*/
