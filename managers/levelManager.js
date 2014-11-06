1f68cad86bf3dd0d71ee15cdaec9925c36a63a12
OddurV <oddurvilhj@gmail.com>
2014-11-06 14:48:44 +0000

    Started a level manager

---
+++ managers/levelManager.js
@@ -0,0 +1,18 @@
+//=============
+//LEVEL MANAGER
+//=============
+/*
+levelManager.js
+
+A module which handles level selection and transition.
+*/
+
+"use strict";
+
+// Tell jslint not to complain about my use of underscore prefixes (nomen),
+// my flattening of some indentation (white), or my use of incr/decr ops 
+// (plusplus).
+//
+/*jslint nomen: true, white: true, plusplus: true*/
+
+var levelManager = {};
