7d2452eaf8a6503b14bdc7de1d35f07320935daa
jiz2 <jiz2@hi.is>
2014-11-10 20:17:50 +0000

    fixed boarder top

--- utils/consts.js
+++ utils/consts.js
@@ -8,10 +8,11 @@
 var consts = {
 
     FULL_CIRCLE: Math.PI * 2,
-    RADIANS_PER_DEGREE: Math.PI / 180.0,
+    RADIANS_PER_DEGREE: Math.PI / 180.0,  
 	
+	wallThickness: 5,
 	wallTop: 30,
 	wallBottom: g_canvas.height - 5,
 	wallLeft: 5,
-	wallRight: g_canvas.width - 5    
+	wallRight: g_canvas.width - 5
 };
