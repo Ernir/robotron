bf18e85a95b7ff509c35c3ca697a43dc05bff827
OddurV <oddurvilhj@gmail.com>
2014-11-13 00:55:28 +0000

    Balanced the levels according to the original game

--- managers/levelManager.js
+++ managers/levelManager.js
@@ -23,16 +23,12 @@ var levelManager = {
     // corresponding type should be created. There is always one protagonist,
     // so we skip him in the level description
     // Key:
-    // Family, Grunts, Hulks, Brains, Electrodes, Quarks (not the Star Trek DS9 version), Spheroids //TODO: add more entities
+    // Family, Electrodes, Grunts, Hulks, Spheroids, Brains, Quarks (not the Star Trek DS9 version)
 
         [], // level "0", skipped automatically
-        [3,6],
-        [5,6,1],
-        [5,4,2,2],
-        [6,6,4,2,4],
-        [6,6,2,1,6,1],
-        [6,10,3,3,6,2,1],
-        [5,5,6,4,8,3,2]
+        [2,0,6],
+        [3,4,8,2],
+        [5,6,10,4]
     ],
 	
 	_isChangingLevel: false,
@@ -53,11 +49,83 @@ var levelManager = {
         //if Player.level > the number of pre-made levels
         //then make a random level
         if (Player.level >= this._levelSpecs.length) {
+
+            var L = Player.level;
             var randomlevel = [];
-            //TODO: update the loop according to the number of enemy + family types
-            for (var i = 0; i < 7; i++) {
-                randomlevel.push(Math.floor(Math.random()*10));
-            };
+
+            switch(true){
+                case (L+1) % 10 === 0:
+                    // Grunt wave
+                    if (g_Debug) console.log("grunts");
+                    randomlevel.push(10); // Family
+                    randomlevel.push(3+Math.floor(L/3)); // Electrodes
+                    randomlevel.push(Math.floor(Math.random()*6)+2*L); // Grunts
+                    randomlevel.push(Math.floor(Math.random()*5)); // Hulks
+                    break;
+                case L % 5 === 0:
+                    // Brain wave (hur hur)
+                    if (g_Debug) console.log("brains");
+                    randomlevel.push(15); // Family
+                    randomlevel.push(3+Math.floor(L/3)); // Electrodes
+                    randomlevel.push(Math.floor(Math.random()*5)+4); // Grunts
+                    randomlevel.push(0); // Hulks
+                    randomlevel.push(0); // Spheroids
+                    randomlevel.push(Math.floor(Math.random()*7)+Math.floor(L/5)); // Brains
+                    break;
+                case (L+3) % 5 === 0:
+                    // Tank wave
+                    if (g_Debug) console.log("tanks");
+                    randomlevel.push(10); // Family
+                    randomlevel.push(0); // Electrodes
+                    randomlevel.push(0); // Grunts
+                    randomlevel.push(3+Math.floor(L/2)); // Hulks
+                    randomlevel.push(0); // Spheroids
+                    randomlevel.push(0); // Brains
+                    randomlevel.push(Math.floor(L/2)); // Quarks
+                    break;
+                case (L+6) % 10 === 0:
+                    // Hulk wave or Enforcer/Tank wave
+                    if (g_Debug) console.log("hulks or enforcers/tanks");
+                    randomlevel.push(10); // Family
+                    randomlevel.push(3+Math.floor(L/3)); // Electrodes
+                    if (Math.random()<0.5) {
+                        randomlevel.push(0); // Grunts
+                        randomlevel.push(0); // Hulks
+                        randomlevel.push(4+Math.floor(L/3)); // Spheroids
+                        randomlevel.push(0); // Brains
+                        randomlevel.push(2+Math.floor(L/4)); // Quarks                        
+                    }else{
+                        randomlevel.push(Math.floor(Math.random()*5)+4); // Grunts
+                        randomlevel.push(6+Math.floor(L/2)); // Hulks
+                        randomlevel.push(Math.floor(Math.random()*3)); // Spheroids
+                    }
+                    break;
+                case L > 27:
+                    // Normal wave + a few Quarks
+                    if (g_Debug) console.log("normal+");
+                    randomlevel.push(10); // Family
+                    randomlevel.push(3+Math.floor(L/3)); // Electrodes
+                    randomlevel.push(Math.floor(Math.random()*6)+L); // Grunts
+                    randomlevel.push(Math.floor(Math.random()*6)+Math.floor(L/10)); // Hulks
+                    randomlevel.push(Math.floor(Math.random()*3)); // Spheroids
+                    randomlevel.push(Math.floor(Math.random()*3)); // Quarks
+                    break;
+                default:
+                    // Normal wave
+                    if (g_Debug) console.log("normal");
+                    randomlevel.push(10); // Family
+                    randomlevel.push(3+Math.floor(L/3)); // Electrodes
+                    randomlevel.push(Math.floor(Math.random()*6)+L); // Grunts
+                    randomlevel.push(Math.floor(Math.random()*6)+Math.floor(L/10)); // Hulks
+                    randomlevel.push(Math.floor(Math.random()*3)); // Spheroids
+            }
+
+
+            if (g_Debug) {
+                console.log("This level consists of:",randomlevel);
+                console.log("Key: [Family, Electrodes, Grunts, Hulks, Spheroids, Brains, Quarks]");
+                console.log("");
+            }
             entityManager.init(randomlevel);
         }else{
             entityManager.init(this._levelSpecs[Player.level]);
@@ -176,7 +244,7 @@ var levelManager = {
 		//score
 		ctx.save();
 		var str = "", hw=g_canvas.width/2 ,hh=g_canvas.height/2;
-		ctx.font = "bold 60px sans-serif"
+		ctx.font = "bold 60px sans-serif";
 		ctx.fillStyle ="rgba(255,255,255,0.3)"; //not sure if you want this
 		ctx.fillRect(0, hh/2, hw*2, hh);
 		ctx.fillStyle ="red";
@@ -184,7 +252,7 @@ var levelManager = {
 		str = "GAME OVER";
 		ctx.fillText(str, hw, hh);
 		str = "Press R to restart";
-		ctx.font = "bold 20px sans-serif"
+		ctx.font = "bold 20px sans-serif";
 		ctx.fillText(str,hw,hh*3/2-10); //10 is the font's halfheight
 		ctx.restore();
 	}
